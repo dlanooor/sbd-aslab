63fc9fae07d1370ef824855ce877e008e50d9b69
Ronald Grant <63400878+dlanooor@users.noreply.github.com>
2021-08-08 00:19:08 +0700

    Add files via upload

---
+++ main/about.php
@@ -0,0 +1,56 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+
+    <link rel="shortcut icon" type="image/png" href="../assets/images/dollar.png"/>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.3.1/jquery.twbsPagination.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../assets/css/style-about.css">
+</head>
+
+<style>
+</style>
+
+<body>
+
+    <nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+		<img src="https://img.icons8.com/bubbles/2x/money.png">
+		</div>
+		<ul class="nav navbar-nav">
+		<li><a href="../index.php">Home</a></li>
+		<li><a href="about.php">About</a></li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+		<ul class="nav navbar-nav navbar-right">
+		<li><a href="../api/registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+		<li><a href="../api/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+		</ul>
+	</div>
+	</nav>
+
+    <div class="about-section">
+        <div class="inner-container">
+            <h1>About Us</h1>
+            <p class="text">
+                Money Management adalah sebuah website yang dapat membantu kita dalam mencatat keuangan kita. Website ini akan memudahkan kita dalam menghitung seberapa banyak income dan outcome yang kita keluarkan, tiap bulannya. Fitur tanggal dan category juga membantu kita dalam menentukan keputusan kedepannya.
+            </p>
+            <div class="skills">
+                <span>Management</span>
+                <span>Budget</span>
+                <span>Invest</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
